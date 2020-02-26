edb9a927f7446dead8652dfede3395b413853b12
krr19499 <kevin.ramos.873@my.csun.edu>
2020-02-26 10:37:38 -0800

    mend

--- senior-app/src/Components/courses.js
+++ senior-app/src/Components/courses.js
@@ -1,36 +1,90 @@
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
+import Selector from 'react-awesome-selector'
+import 'react-awesome-selector/dist/style.css'
+import isEmpty from "lodash/isEmpty";
+import axios from 'axios';
 import Board from 'react-trello';
 
-const data = {
-    lanes: [
-      {
-        id: 'lane1',
-        title: 'Courses',
-        style:{backgroundColor:'#343a40', color:'white', textAlign:'center' },
-        cards: [
-          {id: 'Card1', title: 'COMP 110', },
-          {id: 'Card2', title: 'COMP 100'}
-        ]
-      },
-      {
-        id: 'lane2',
-        style:{backgroundColor:'#343a40', color:'white', textAlign:'center'},
-        title: 'Current Courses',
-        cards: []
-      }
-    ] 
-
-  }
+const classes=[{}];
 class courses extends Component{
-    render(){
-        return(
-            <Board 
-            style={{backgroundColor:'#6c757d', fontFamily:'Verdana'}}
-            cardStyle={{backgroundColor:'silver', textAlign:'center'}}
-            hideCardDeleteIcon={'true'}
-            data={data}/>
+  
+  constructor(props, context){
+    super(props, context);
+    this.state={
+      test:[],
+      another:[]
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/api/senior/universities/californiastateuniversitynorthridge?format=json")
+      .then(response =>{
+        const classes = response.data;
+        const id =response.data.map(input=>input.californiastateuniversitynorthridge_id);
+        this.setState({
+          test: classes,
+          another: id
+        },
+        function(){console.log("response",this.state.test, this.state.another)});
+        
+      })
+      
+      .catch(error => this.setState({ error }));
+  };
+
+  setTable(){
+    const card=this.state.test.filter(input=>input.californiastateuniversitynorthridge_courses!==null);
+    const data ={
+      lanes: [
+        {
+          id: "Courses",
+          title: "Courses",
+          style:{backgroundColor:'#343a40', color:'white', textAlign:'center' },
+          cards:card.map(input=>({
+            id:input.californiastateuniversitynorthridge_id.toString(),
+            title:input.californiastateuniversitynorthridge_courses
+          }))
+          
+         
+        },
+        {
+          id: 'lane2',
+          style:{backgroundColor:'#343a40', color:'white', textAlign:'center'},
+          title: 'Current Courses',
+          cards: []
+        }
+      ]
+    };
+
+    return(
+       <Board  
+        style={{backgroundColor:'#6c757d', fontFamily:'Verdana'}}
+        selectedTitle='Current Courses'
+         data={data} 
+      />)
+  }
+	
+  render(){
+    //var classes = this.state.test.map(input=>input.input.californiastateuniversitynorthridge_courses);
+    const something = this.setTable();
+    console.log("classes",this.state.test)    
+        return( 
+          <div>
+            
+          <h1>Something</h1>
+          {something}
+            </div>
+           //            {this.state.test.map(input=><div>{input}</div>)}
+
+           // <Board  
+           // style={{backgroundColor:'#6c757d', fontFamily:'Verdana'}}
+            //selectedTitle='Current Courses'
+          //    data={classes} 
+           // > 
         )
     }
+    
 }
+
 
 export default courses;
